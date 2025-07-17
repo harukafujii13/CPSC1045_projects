6c34d28b27d6b20fd9ce26b70fc99fa5111869d9
maco <tanoshiikaidan@gmail.com>
2025-07-17 15:38:31 -0700

    add files

---
+++ Assignment_1/index.js
@@ -0,0 +1,32 @@
+let board = [];
+
+for (let row = 0; row < 8; row++) {
+  board[row] = [];
+
+  for (let col = 0; col < 8; col++) {
+    if (row < 3 && (row + col) % 2 === 1) {
+      board[row][col] = "gray";
+    } else if (row > 4 && (row + col) % 2 === 1) {
+      board[row][col] = "red";
+    } else {
+      board[row][col] = "";
+    }
+  }
+}
+
+
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+
+function drawPieces() {
+  for (let x = 0; x < board.length; x++) {
+    for (let y = 0; y < board[y].length; y++) {
+      if (board[x][y] == "red" || board[x][y] == "gray") {
+        ctx.fillStyle = board[x][y];
+        ctx.beginPath();
+        ctx.arc(x * 100 + 50,  y * 100 + 50, 35, 0, 2 * Math.PI);
+        ctx.fill();
+      }
+    }
+  }
+}
